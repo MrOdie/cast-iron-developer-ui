96b2b60beaebd19a385e1d8cbf8eb5e1e2780698
MrOdie <sodonnell5214@gmail.com>
2022-07-25 20:07:49 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# UI for Cast Iron Dev
---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+  export let name;
+</script>
+
+<h1>Hello {name}!</h1>
+
+<div>
+  <p>SASS is working!</p>
+</div>
+
+<style lang="scss">
+  $color: red;
+
+  h1 {
+    color: $color;
+  }
+
+  div {
+    background: green;
+
+    > p {
+      color: #fff;
+    }
+  }
+</style>
+
